66bf77d662fa36c07c3ce36db8c8cc886e52e208
lusacramento <lucianossacramento@yahoo.com.br>
2024-01-30 16:18:54 -0300

    install googlefont module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ['@nuxtjs/tailwindcss'],
+	modules: [
+		'@nuxtjs/tailwindcss',
+		[
+			'@nuxtjs/google-fonts',
+			{
+				families: {
+					Roboto: [300, 400, 500, 700],
+				},
+			},
+		],
+	],
 	css: ['~/assets/css/main.css'],
 })
