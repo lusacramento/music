5fdaa4f157b294766921fa804309abf84dfa8c2b
lusacramento <lucianossacramento@yahoo.com.br>
2024-02-06 10:03:27 -0300

    feat icon component

---
+++ components/AppIcon.vue
@@ -0,0 +1,14 @@
+<template>
+	<ClientOnly>
+		<font-awesome-icon :icon="icon" :class="class" />
+	</ClientOnly>
+</template>
+
+<script lang="ts" setup>
+	const props = defineProps({
+		icon: { type: String, required: true },
+		class: { type: String, required: true, default: '' },
+	})
+</script>
+
+<style></style>
