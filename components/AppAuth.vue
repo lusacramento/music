c988be03dc0119cdc4fb6fa65af8af7f4c42f808
lusacramento <lucianossacramento@yahoo.com.br>
2024-02-06 20:22:32 -0300

    define validation rules

--- components/AppAuth.vue
+++ components/AppAuth.vue
@@ -69,6 +69,61 @@
 </template>
 
 <script lang="ts" setup>
+	import { defineRule, configure } from 'vee-validate'
+
+	import {
+		required,
+		min,
+		min_value as minValue,
+		max,
+		max_value as maxValue,
+		alpha_spaces as alphaSpaces,
+		email,
+		confirmed,
+		not_one_of as excluded,
+	} from '@vee-validate/rules'
+
+	defineRule('required', required)
+	defineRule('tos', required)
+	defineRule('min', min)
+	defineRule('min_value', minValue)
+	defineRule('max', max)
+	defineRule('max_value', maxValue)
+	defineRule('alpha-spaces', alphaSpaces)
+	defineRule('email', email)
+	defineRule('passwords_mismatch', confirmed)
+	defineRule('excluded', excluded)
+	defineRule('country_excluded', excluded)
+
+	configure({
+		generateMessage: (ctx) => {
+			const messages = {
+				required: `The field ${ctx.field} is required.`,
+				min: `The field ${ctx.field} mis too short.`,
+				max: `The field  ${ctx.field} is too long.`,
+				alpha_spaces: `The field ${ctx.field} may only contain alphabetical characters and spaces.`,
+				email: `The field ${ctx.field} must bee a valid email.`,
+				min_value: `The field${ctx.field} is too low.`,
+				max_value: `The  field ${ctx.field} is too high.`,
+				excluded: `You are not allowed to use this value for the field ${ctx.field}.`,
+				country_excluded: `Due to restrictions, we donot accept users from this location.`,
+				passwords_mismatch: `The passwords don't match.`,
+				tos: `You must accept the Terms of Service.`,
+			}
+
+			const message = messages[ctx.rule?.name as keyof typeof messages]
+				? messages[ctx.rule?.name as keyof typeof messages]
+				: `The field ${ctx.field} is invalid.`
+
+			return message
+		},
+
+		validateOnBlur: true,
+		validateOnChange: true,
+		validateOnInput: false,
+		validateOnModelUpdate: true,
+	})
+
 	const hiddenClass = computed(() => {
 		return useMyModalStore().hiddenClass
 	})
