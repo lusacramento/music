7329d6fa1eba997b5d89499fb47338e7735921d7
lusacramento <lucianossacramento@yahoo.com.br>
2024-03-05 18:57:07 -0300

    set id in order select component

--- pages/song/[id].vue
+++ pages/song/[id].vue
@@ -64,6 +64,7 @@
 					</VeeForm>
 					<!-- Sort Comments -->
 					<select
+						id="order-select"
 						class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
 						v-model="order"
 					>
