e2b2f2ac87155a364fdc70ebc4c75f838239aa6f
lusacramento <lucianossacramento@yahoo.com.br>
2024-03-14 11:08:01 -0300

    implements comments store

---
+++ stores/comments.ts
@@ -0,0 +1,54 @@
+import { addDoc, getDocs, orderBy, query, where } from 'firebase/firestore'
+import { defineStore } from 'pinia'
+
+export const useMyCommentsStore = defineStore({
+	id: 'myCommentsStore',
+	state: () => ({
+		comments: [] as any[],
+	}),
+
+	getters: {
+		getComments: function (state) {
+			return state.comments
+		},
+	},
+
+	actions: {
+		async getCommentsOnStore(id: string) {
+			try {
+				const commentsColection = useNuxtApp().$commentsCollection
+
+				const collectionRef = commentsColection
+				const queryRef = query(
+					collectionRef,
+					where('sid', '==', id),
+					orderBy('datePosted', 'desc'),
+				)
+
+				const snapshot = await getDocs(queryRef)
+				snapshot.forEach(async (doc) => {
+					const currentComment: any = await {
+						id: doc.id,
+						...doc.data(),
+					}
+					currentComment.datePosted = new Date(currentComment.datePosted * 1000)
+					this.comments.push({
+						...currentComment,
+					})
+				})
+			} catch (error) {
+				console.log(error)
+			}
+		},
+
+		async addCommentOnStore(comment: any) {
+			try {
+				const commentsCollection = useNuxtApp().$commentsCollection
+
+				await addDoc(commentsCollection, comment).then(async () => {
+					this.comments.push(comment)
+				})
+			} catch (error) {}
+		},
+	},
+})
